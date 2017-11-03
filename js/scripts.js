1133e00ff8ab3caf82c6af0440cdf68213137972
elena-castello <31969907+elena-castello@users.noreply.github.com>
2017-11-03 17:24:39 -0400

    Attempt 1

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){  // do not delete
+// EXERCISE 2
+
+// this part is to resize the "viewer" div for mobile only
+// it works for images with a 4:3 aspect ratio - specifically 800 x 600
+var viewerWidth = $( '#slide' ).width();
+if (viewerWidth < 1280) {
+	$( '#slide' ).height( viewerWidth * 0.75 );
+}
+
+// this adjusts the height of the "viewer" div dynamically as the user
+// resizes the browser window
+$( window ).resize(function() {
+	var viewerWidth = $( '#slide' ).width();
+	if (viewerWidth < 1280) {
+		$( '#slide' ).height( viewerWidth * 0.75 );
+	}
+});
+
+// this makes the clicked image fade out and hide
+// also, the faded image is moved with .append() to the end of the queue
+// to make the slideshow endless
+$('.fadeitem').on('click', function() {
+	$(this).fadeOut('slow', function() {
+		// move it to the end of the stack of photos
+		$('#slide').append(this);
+		// make it visible again
+		$(this).fadeIn('fast');
+	});
+});
+
+}); // do not delete
